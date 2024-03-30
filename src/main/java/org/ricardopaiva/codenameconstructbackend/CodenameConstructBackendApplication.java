0f2265739f0b8534be7be8d06f6f544b3e8e9452
Ricardo Paiva <ricardoclpaiva@gmail.com>
2024-03-29 22:50:44 -0300

    Initial commit

---
+++ src/main/java/org/ricardopaiva/codenameconstructbackend/CodenameConstructBackendApplication.java
@@ -0,0 +1,13 @@
+package org.ricardopaiva.codenameconstructbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodenameConstructBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodenameConstructBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ricardopaiva/codenameconstructbackend/CodenameConstructBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ricardopaiva.codenameconstructbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodenameConstructBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
